5406ed1ff15b5452c6f7c3747ad3ce43208b6a40
Aubrey <aubrey.goodman@gmail.com>
2013-01-24 17:24:09 -0500

    updated style with some visual design

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
 	$('.stat > .chicken img').click(function() {
 		$('#vote_chicken')[0].checked = true;
 		$('#vote_cobra')[0].checked = false;
+		$('#drawer').show();
 	});
 	$('.stat > .cobra img').click(function() {
 		$('#vote_chicken')[0].checked = false;
 		$('#vote_cobra')[0].checked = true;
+		$('#drawer').show();
 	});
 });
