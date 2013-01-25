22ca008cea8b944be0c64cc233ff4ac42f0db200
Aubrey <aubrey.goodman@gmail.com>
2013-01-25 16:55:27 -0500

    fixed jquery bug assigning vote value

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 	$('#chicken').click(function() {
-		$('#vote_chicken')[0].checked = true;
-		$('#vote_cobra')[0].checked = false;
+		$('#vote_chicken').val(true);
+		$('#vote_cobra').val(false);
 		$('#drawer').show();
 		$('#chicken').addClass('active');
 		$('#cobra').removeClass('active');
 	});
 	$('#cobra').click(function() {
-		$('#vote_chicken')[0].checked = false;
-		$('#vote_cobra')[0].checked = true;
+		$('#vote_chicken').val(false);
+		$('#vote_cobra').val(true);
 		$('#drawer').show();
 		$('#chicken').removeClass('active');
 		$('#cobra').addClass('active');
