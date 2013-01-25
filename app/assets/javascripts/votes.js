e5401eea2bef0dc013ecd1acd316a633a96331ba
Aubrey <aubrey.goodman@gmail.com>
2013-01-25 09:31:56 -0500

    added sprite image and more style

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
-	$('.stat > .chicken img').click(function() {
+	$('#chicken').click(function() {
 		$('#vote_chicken')[0].checked = true;
 		$('#vote_cobra')[0].checked = false;
 		$('#drawer').show();
+		$('#chicken').addClass('active');
+		$('#cobra').removeClass('active');
 	});
-	$('.stat > .cobra img').click(function() {
+	$('#cobra').click(function() {
 		$('#vote_chicken')[0].checked = false;
 		$('#vote_cobra')[0].checked = true;
 		$('#drawer').show();
+		$('#chicken').removeClass('active');
+		$('#cobra').addClass('active');
 	});
 });
