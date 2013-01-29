82b2447c01086dc8d7c1198a39cad81e8fb945d3
Aubrey <aubrey.goodman@gmail.com>
2013-01-28 19:07:03 -0500

    upgraded to white rounded health bars; moved share buttons to top of header

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 	$('#chicken').click(function() {
 		$('#vote_chicken').val(true);
 		$('#vote_cobra').val(false);
-		$('#drawer').show();
+		$('#drawer').show({slide: 'down'});
 		$('#chicken').addClass('active');
 		$('#cobra').removeClass('active');
 	});
 	$('#cobra').click(function() {
 		$('#vote_chicken').val(false);
 		$('#vote_cobra').val(true);
-		$('#drawer').show();
+		$('#drawer').show({slide: 'down'});
 		$('#chicken').removeClass('active');
 		$('#cobra').addClass('active');
 	});
